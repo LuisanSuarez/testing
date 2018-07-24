c993d09e4b7c06ae41eee8d3c4a54b75f6dc6909
Luis Angel <luisangelsc@gmail.com>
2018-07-24 13:38:27 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const strings1 = ['short', 'first long string!!', 'medium', 'abcdefgisdflmnopqdr'];
+
+
+function longestString(arr) {
+  let ans = arr[0]
+  for (i=1; i<arr.length; i++){
+    arr[i].length > ans.length ? ans = arr[i] : ''
+  }
+
+  console.log(ans)
+}
+
+longestString(strings1)
+
+console.log(strings1[1].length );
+console.log(strings1[3].length);
